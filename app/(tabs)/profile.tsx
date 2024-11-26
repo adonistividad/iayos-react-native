31438c44f71426d762aeebe3c958153e9896d52c
Adonis Tividad <adonis@spls.ae>
2024-11-26 18:56:19 +0400

    UPDATE pages

--- app/(tabs)/profile.tsx
+++ app/(tabs)/profile.tsx
@@ -1,14 +1,20 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import { Link } from "expo-router";
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
 
-const profile = () => {
+const Profile = () => {
   return (
-    <View>
-      <Text>profile</Text>
-    </View>
-  )
-}
+    <SafeAreaView className="flex-1 items-center justify-center bg-dark">
+      <Text className="color-white">Profile</Text>
+      <StatusBar style="auto" />
+      <Link href={"/"} style={{ color: "blue" }}>
+        Go to Home
+      </Link>
+    </SafeAreaView>
+  );
+};
 
-export default profile
+export default Profile;
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({});
